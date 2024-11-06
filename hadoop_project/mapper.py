445f13d21fdeb933afc835d9a7e9ac1176aa6b03
Jabulani-N <jabu.94@gmail.com>
2024-11-05 22:07:31 -0600

    task 6 passes local test, trims dupes, and only counts up to 30 becuase we don't need 10k printed lines

--- hadoop_project/mapper.py
+++ hadoop_project/mapper.py
@@ -5,17 +5,24 @@ import csv
 
 # Read from standard input
 reader = csv.reader(sys.stdin,delimiter=',')
+counter = 0
+completed_ids = []
 for line in reader:
     # Strip whitespace and split the line into fields
     # line = line.strip()
 
     try:
         # Extract the fields from the CSV
-        # id, company, totalyearlycompensation = line
+        id, company, totalyearlycompensation = line[0], line[1], line[3]
+        if id not in completed_ids:
+            # Print the output in the required format
+            print(f"{id}\t{company},{totalyearlycompensation}")
+            completed_ids.append(id)
+            counter += 1
 
-        # Print the output in the required format
-        print(f"{line[0]}\t{line[1]},{line[2]}")
-    except ValueError:
-        # if line does not have enough fields
-        print("something failed")
-        continue
+    except IndexError:
+        # if line does not have correct fields
+        print("missing field")
+        # continue
+    # there are thousands of entries, we don't need to print all
+    if counter > 30: break
